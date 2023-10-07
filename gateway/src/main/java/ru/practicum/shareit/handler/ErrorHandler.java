290df0a943b3ac23047915d9d0c0aa7dcfd2e539
Canoutis <rosoutis@gmail.com>
2023-10-07 22:01:22 +0300

    fix: Validation handler

--- gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +24,13 @@ public class ErrorHandler {
     public ErrorResponse handleOtherExceptions(Throwable ex) {
         log.error("Internal Server Error: " + ex.getMessage());
         return new ErrorResponse("Internal Server Error", "У нас что-то сломалось! Честно чиним!");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+        log.error("Validation failed: " + ex.getMessage());
+        return new ErrorResponse("Invalid input", "Ошибка валидации входных данных.");
     }
 }
